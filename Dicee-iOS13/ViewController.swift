281bd658ff54db86b4009aa23223059a5519415e
Igor Amantino <igor.amantino.ext@dasa.com>
2022-09-11 17:59:50 -0300

    DiceIOS

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   //podemos importa um component diretamente no codigo apertando e segurando o "option" e arrastando a linha para o codigo
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    
+    //podemos acessar a image usando diretamente a pasta assets usando #imageLiteral() exemplo diceImageViewTwo.image = #imageLiteral(nameImage)
+    let imageOne =  UIImage(imageLiteralResourceName: "DiceOne")
+    let imageTwo =  UIImage(imageLiteralResourceName: "DiceTwo")
+    let imageThree =  UIImage(imageLiteralResourceName: "DiceThree")
+    let imageFour =  UIImage(imageLiteralResourceName: "DiceFour")
+    let imageFive =  UIImage(imageLiteralResourceName: "DiceFive")
+    let imageSix =  UIImage(imageLiteralResourceName: "DiceSix")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        }
+
+    
+    @IBAction func playButtonPress(_ sender: UIButton) {
+        let imageRandom  = [imageOne,imageTwo,imageFive,imageThree,imageFour,imageSix]
+        
+        diceImageViewOne.image = imageRandom.randomElement()
+        diceImageViewTwo.image = imageRandom.randomElement()
     }
-
-
 }
+
+
+
+
 
